578756218591f61b96c22dcda16d25f89a84e08a
MarcE <marc.ellis.dev@gmail.com>
2025-12-24 10:59:11 +1300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "love-message-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache first
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
