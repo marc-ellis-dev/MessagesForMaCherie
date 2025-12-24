f81a18f4bc2671afe98bc4a5f0154a02d9a86ea6
MarcE <marc.ellis.dev@gmail.com>
2025-12-25 12:52:37 +1300

    More

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = "ma-cherie-v1";
+const CACHE_VERSION = "v2";
+const CACHE_NAME = `ma-cherie-${CACHE_VERSION}`;
+
 const urlsToCache = [
   "/",
   "/index.html",
@@ -10,6 +12,17 @@ const urlsToCache = [
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
 
